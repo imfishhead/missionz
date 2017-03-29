5934ae96c4cd7e7c3780dda38c5995c0eba3a8ad
Adele Yu <fhead17@gmail.com>
2017-03-29 11:19:53 +0800

    add fallback message

--- app/models/blessing.rb
+++ app/models/blessing.rb
@@ -1,5 +1,5 @@
 class Blessing < ActiveRecord::Base
-	scope :solved -> { where(solved: true) }
-	scope :unsolved -> { where(solved: false) }
+	scope :solved, -> { where(solved: true) }
+	scope :unsolved, -> { where(solved: false) }
 	mount_uploader :photo, AvatarUploader
 end
